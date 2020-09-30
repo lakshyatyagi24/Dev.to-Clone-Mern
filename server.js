623fb211f89b4bd7c23bb03ca882cf167ad53eeb
DucmaD <hieukio1159@gmail.com>
2020-09-30 08:43:11 +0700

    ....

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.json());
 connectDB();
 
+// route
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
